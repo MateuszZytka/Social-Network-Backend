48fb64f9bd084e4585d0e5c42dbf39ae3a184154
MateuszZytka <109117198+MateuszZytka@users.noreply.github.com>
2022-11-09 17:45:45 -0500

    completed models

---
+++ models/Thought.js
@@ -0,0 +1,71 @@
+const { Schema, model, Types } = require('mongoose');
+
+// Schema to create Thoughts model
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: "Text is required",
+            match: '/^.{1,280}$/'
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        get:() =>{
+            let date = new Date();
+            return date.toLocaleString();
+        },
+        username: { type: String, required: true },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+      }
+)
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            match: '/^.{1,280}$/'
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        get:() =>{
+            let date = new Date();
+            return date.toLocaleString();
+        },
+    },
+    {
+        toJSON: {
+          getters: true,
+        },
+        id: false,
+      }
+)
+
+// a virtual that returns the count of objects in the reactions array
+thoughtSchema.virtual('virtualCOunt').get(function(){
+    return `${this.reactions.length}`
+})
+
+//initialize our Thoughts model
+const Thoughts = model('thoughts', thoughtSchema)
+
+module.exports = Thoughts
---
+++ models/User.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: "Username is required",
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: "Email is required",
+            unique: true,
+            trim: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        },
+        thoughts: [thoughtSchema],
+        friends: [friendSchema],
+    },
+    {
+        toJSON: {
+          virtuals: true,
+        },
+        id: false,
+      }
+)
+
+const User = model('user', userSchema)
+
+module.exports = User
